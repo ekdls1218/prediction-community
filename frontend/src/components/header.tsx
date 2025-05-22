0efc1fd3a240ca6d3cbe69fc16f7ef7fa377c83f
김다인 <dbslzhs1218@naver.com>
2025-05-22 10:50:56 +0900

    style: 헤더 css 수정

--- frontend/src/components/header.tsx
+++ frontend/src/components/header.tsx
@@ -1,8 +1,8 @@
 export default function Header() {
   return (
-    <div className="my-5 pb-5 flex items-center justify-between px-6">
+    <div className="py-5 flex items-center justify-between px-6">
       <h1 className="font-pyeongchang font-bold text-5xl text-primary-color">과연 맞을까?</h1>
-      <nav className="flex space-x-6 text-gray-700 text-lg">
+      <nav className="flex space-x-4 text-gray-700 text-lg">
         <a href="/login" className="hover:text-primary-color">로그인</a>
         <a href="/signup" className="hover:text-primary-color">회원가입</a>
       </nav>
