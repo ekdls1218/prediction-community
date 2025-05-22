bfae4f7ff05c12f0c0df6b354be0b3ab8cf72784
김다인 <dbslzhs1218@naver.com>
2025-05-22 10:30:56 +0900

    feat: 폰트 설정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,10 +1,31 @@
 import Header from "@/components/header"
+import localFont from "next/font/local"
+import "./globals.css"
+
+const pyeongChangPeace = localFont({
+  src: [
+    {
+      path: "../../public/fonts/PyeongChangPeace-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/PyeongChangPeace-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-pyeongchang",
+  display: "swap",
+});
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ko">
+    <html lang="ko" className={pyeongChangPeace.variable}>
       <body>
-        <Header />
+        <header className="font-pyeongchang">
+          <Header />
+        </header>
         <main>{children}</main>
       </body>
     </html>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        pyeongchang: ["var(--font-pyeongchang)"],
+      },
+      colors: {
+        "primary-color": '#634282',
+        "secondary-color": '#edd9ff',
+      },
+    },
   },
   plugins: [],
-}
-
+};
