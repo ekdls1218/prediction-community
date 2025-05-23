e0adf64858f25d42efad210a9cdaf601818e6198
김다인 <dbslzhs1218@naver.com>
2025-05-23 14:01:26 +0900

    feat: 중복 체크 및 회원가입 엔드포인트 설정

---
+++ backend/homeController.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from user.userDAO import UserDAO
+
+# uvicorn homeController:app --host=localhost --port=8000 --reload
+app = FastAPI()
+uDao = UserDAO()
+
+@app.get("/auth/check-nick")
+def checkNick(nick):
+    return uDao.checkNick(nick)
+
+@app.get("/auth/check-id")
+def checkId(id):
+    return uDao.checkId(id)
+
+@app.post("/auth/signup")
+def signUp(nick):
+    return uDao.checkNick(nick)
