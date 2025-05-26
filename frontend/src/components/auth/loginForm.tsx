a890a3f42c8a6dfeb315f2b630abbb9bac658e6b
김다인 <dbslzhs1218@naver.com>
2025-05-26 18:21:53 +0900

    feat: [FE]로그인 요청

--- frontend/src/components/auth/loginForm.tsx
+++ frontend/src/components/auth/loginForm.tsx
@@ -1,5 +1,8 @@
 "use client";
-import { useState } from "react";
+import { isEmpty } from "@/validators";
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import { useRef, useState } from "react";
 
 interface Login {
   id: string;
@@ -7,13 +10,68 @@ interface Login {
 }
 
 export default function LoginForm() {
+  const router = useRouter();
   const [loginInfo, setLoginInfo] = useState<Login>({ id: "", pw: "" });
-  console.log(loginInfo)
+  //   console.log(loginInfo);
 
-  const changeInput = (e:React.ChangeEvent<HTMLInputElement>) => {
-    const {name, value} = e.target;
-    setLoginInfo({...loginInfo, [name]:value})
-  }
+  const changeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setLoginInfo({ ...loginInfo, [name]: value });
+  };
+
+  const inputRef = useRef<Record<string, HTMLInputElement | null>>({});
+  const loginFormData = new FormData();
+
+  const makeFormData = () => {
+    for (const [key, value] of Object.entries(loginInfo)) {
+      loginFormData.append(key, value);
+    }
+  };
+
+  const validCheck = () => {
+    if (isEmpty(loginInfo.id) && inputRef.current.id) {
+      alert("아이디를 다시 입력해주세요.");
+      inputRef.current.id.value = "";
+      inputRef.current.id.focus();
+      return false;
+    }
+    if (isEmpty(loginInfo.pw) && inputRef.current.pw) {
+      alert("비밀번호를 다시 입력해주세요.");
+      inputRef.current.pw.value = "";
+      inputRef.current.pw.focus();
+      return false;
+    }
+    return true;
+  };
+
+  const login = () => {
+    if (validCheck()) {
+      makeFormData();
+      axios
+        .post("http://localhost:8000/auth/login", loginFormData, {
+          headers: { "Content-Type": "multipart/form-data" },
+          withCredentials: true,
+        })
+        .then((res) => {
+            console.log(res.data)
+          if (res.data.result === "로그인 성공") {
+            router.push("/");
+          }else if(res.data.result === "로그인 실패(아이디)") {
+            alert("잘못된 아이디입니다. 다시 입력해주세요.")
+            if (inputRef.current.id) {
+                inputRef.current.id.value = "";
+                inputRef.current.id.focus();
+            }
+          }else if(res.data.result === "로그인 실패(비밀번호)") {
+            alert("잘못된 비밀번호입니다. 다시 입력해주세요.")
+            if (inputRef.current.pw) {
+                inputRef.current.pw.value = "";
+                inputRef.current.pw.focus();
+            }
+          }
+        });
+    }
+  };
 
   return (
     <div className="my-10 text-gray-700 w-full max-w-sm">
@@ -23,7 +81,11 @@ export default function LoginForm() {
           <input
             type="text"
             placeholder="아이디를 입력해주세요."
+            name="id"
             onChange={changeInput}
+            ref={(thisInput) => {
+              inputRef.current.id = thisInput;
+            }}
             className="flex-1 border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-primary-color"
           />
         </div>
@@ -31,11 +93,18 @@ export default function LoginForm() {
           <input
             type="password"
             placeholder="비밀번호를 입력해주세요."
+            name="pw"
             onChange={changeInput}
+            ref={(thisInput) => {
+              inputRef.current.pw = thisInput;
+            }}
             className="flex-1 border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-primary-color"
           />
         </div>
-        <button className="w-full py-3 mt-4 bg-primary-color text-white rounded-md font-semibold ">
+        <button
+          onClick={login}
+          className="w-full py-3 mt-4 bg-primary-color text-white rounded-md font-semibold "
+        >
           로그인
         </button>
       </div>
