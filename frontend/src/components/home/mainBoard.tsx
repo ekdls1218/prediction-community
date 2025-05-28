c103a17e13824dbd8b99145d18dc43a5e5516f9b
김다인 <dbslzhs1218@naver.com>
2025-05-28 21:26:08 +0900

    style: [FE]메인보드 css 수정

--- frontend/src/components/home/mainBoard.tsx
+++ frontend/src/components/home/mainBoard.tsx
@@ -7,7 +7,7 @@ export default function MainBoard() {
     const [isOpenModal, setIsOpenModal] = useState(false);
     
     return(
-        <div className="h-screen h-full flex flex-col w-3/5 bg-white border-2 border-gray-200 rounded-xl p-4">
+        <div className="h-full flex flex-col w-3/5 bg-white border-2 border-gray-200 rounded-xl p-4">
             <div className="h-12 flex items-baseline">
                 <h1 className="text-2xl font-bold text-gray-900 mr-4">예측하기</h1>
                 <button onClick={() => setIsOpenModal(true)} className="bg-primary-color text-white text-sm font-semibold py-1.5 px-3 rounded-md">새 예측 작성</button>
