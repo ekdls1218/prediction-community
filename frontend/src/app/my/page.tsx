a873e3c882f216a034cae8f797fa4fed1e0d5c01
김다인 <dbslzhs1218@naver.com>
2025-05-30 16:53:08 +0900

    refactor: [FE]마이페이지 구조 변경

--- frontend/src/app/my/page.tsx
+++ frontend/src/app/my/page.tsx
@@ -1,10 +1,8 @@
 import MyMain from "@/components/my/MyMain";
-import NaviSideBar from "@/components/my/NaviSideBar";
 
 export default function MyPage() {
   return (
-    <div className="mx-auto max-w-[1200px] h-screen px-5 py-8 grid grid-cols-1 md:grid-cols-[280px_1fr] gap-6">
-      <NaviSideBar />
+    <div className="flex justify-center w-full h-screen px-5 py-8 bg-gray-50">
       <MyMain />
     </div>
   );
