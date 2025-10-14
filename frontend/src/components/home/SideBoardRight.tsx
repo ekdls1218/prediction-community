45391b93452059b2cf44aeeb2f94a80c43446fe2
김다인 <dbslzhs1218@naver.com>
2025-10-14 12:54:28 +0900

    feat: [FE]인기 게시글 top3 보여주기

--- frontend/src/components/home/SideBoardRight.tsx
+++ frontend/src/components/home/SideBoardRight.tsx
@@ -3,18 +3,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-interface CountRank {
+interface UserVoteRank {
     nick: string;
     total_count:number;
 }
 
+interface PostVoteRank {
+    title: string;
+}
 
 export default function SideBoardRight() {
-    const [countRank, setCountRank] = useState<CountRank[]>([])
+    const [userVoteRank, setUserVoteRank] = useState<UserVoteRank[]>([])
+    const [postVoteRank, setPostVoteRank] = useState<PostVoteRank[]>([])
     useEffect(() => {
         axios.get("http://localhost:8000/rank/user").then((res) => {
             console.log(res.data)
-            setCountRank(res.data)
+            setUserVoteRank(res.data)
+        })
+
+        axios.get("http://localhost:8000/rank/post").then((res) => {
+            console.log(res.data)
+            setPostVoteRank(res.data)
         })
     }, [])
     return(
@@ -23,18 +32,27 @@ export default function SideBoardRight() {
                 <h3 className="text-lg font-semibold text-gray-800 pl-1 mb-2">
                     인기 예측 글 top3
                 </h3>
-                    
+                {postVoteRank.map((r,i) => {
+                    return(
+                        <div key={i} className="border-2 border-gray-100 text-center shadow-sm rounded-md px-2 py-1 bg-white mb-2">
+                            <span className="block text-xs text-gray">{i+1}등</span>
+                            <span className="text-primary-color text-md font-bold mb-2">
+                            {r.title}
+                            </span>
+                        </div>
+                    )
+                })}            
             </div>
             <div>
                 <h3 className="text-lg font-semibold text-gray-800 pl-1 mb-2">
                     최다 참여자 top3
                 </h3>
-                {countRank.map((r,i) => {
+                {userVoteRank.map((r,i) => {
                     return(
                         <div key={i} className="border-2 border-gray-100 text-center shadow-sm rounded-md px-2 py-1 bg-white mb-2">
                             <span className="block text-xs text-gray">{i+1}등</span>
                             <span className="text-primary-color text-md font-bold mb-2">
-                            {r.nick} ({r.total_count}개)
+                            {r.nick} ({r.total_count}표)
                             </span>
                         </div>
                     )
